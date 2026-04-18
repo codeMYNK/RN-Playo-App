bf2349c61ddbd21fd165e9036b2ff450660a23b5
Mayank Kumar <mynk8076@gmail.com>
2026-04-18 22:21:05 +0530

    Paused

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,14 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Pressable,
+  Image,
+} from "react-native";
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { Bell, MessageCircle } from "lucide-react-native";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="flex-1 bg-white">
+      <View className="px-4 py-3 bg-white flex-row justify-between items-center border-b border-gray-200">
+        <View className="flex-1">
+          <Text className="text-gray-400 text-xs">Location</Text>
+          <Text className="text-lg font-semibold">Sahakar Nagae, Bengluru</Text>
+        </View>
 
-export default HomeScreen
+        <View className="flex-row gap-4 items-center ml-2">
+          <MessageCircle size={20} stroke="#333" />
+          <Bell size={20} stroke="#333" />
+          <Pressable>
+            <Image
+              source={{
+                uri: "https://lh3.googleusercontent.com/ogw/AF2bZyhUsU576pbriS53p-a_4Ecu-odElHuco4vA0al1_A61wQ=s32-c-mo",
+              }}
+            />
+          </Pressable>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
